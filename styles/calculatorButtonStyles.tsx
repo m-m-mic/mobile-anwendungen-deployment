736a5f16268390bab870d58d3d36040c8ffbc4dc
Michael <michael.j.buchholz@t-online.de>
2023-06-18 15:11:38 +0200

    changed button color to red

--- styles/calculatorButtonStyles.tsx
+++ styles/calculatorButtonStyles.tsx
@@ -34,7 +34,7 @@ export const calculatorButtonStyles = StyleSheet.create({
     color: "#3d3d3d",
   },
   input: {
-    backgroundColor: "#ffffff",
+    backgroundColor: "#ff0000",
   },
   operator: {
     backgroundColor: "#8747f1",
